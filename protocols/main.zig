a967d9924e7368c637331052d3098ed92d653762
Nick Erdmann <n@nirf.de>
2019-10-30 12:46:00 +0100

    wip

--- protocols/main.zig
+++ protocols/main.zig
@@ -16,6 +16,38 @@ fn printf(buf: []u8, comptime format: []const u8, args: ...) void {
     puts(fmt.bufPrint(buf, format, args) catch unreachable);
 }
 
+pub const SimpleNetworkProtocol = extern struct {
+    revision: u64,
+    _start: extern fn (*const SimpleNetworkProtocol) usize,
+    _stop: extern fn (*const SimpleNetworkProtocol) usize,
+    _initialize: extern fn (*const SimpleNetworkProtocol, usize, usize) usize,
+    // TODO
+
+    /// Changes the state of a network interface from "stopped" to "started".
+    pub fn start(self: *const SimpleNetworkProtocol) usize {
+        return self._start(self);
+    }
+
+    /// Changes the state of a network interface from "started" to "stopped".
+    pub fn stop(self: *const SimpleNetworkProtocol) usize {
+        return self._stop(self);
+    }
+
+    /// Resets a network adapter and allocates the transmit and receive buffers required by the network interface.
+    pub fn initialize(self: *const SimpleNetworkProtocol, extra_rx_buffer_size: usize, extra_tx_buffer_size: usize) usize {
+        return self._initialize(self, extra_rx_buffer_size, extra_tx_buffer_size);
+    }
+
+    pub const guid align(8) = uefi.Guid{
+        .time_low = 0xa19832b9,
+        .time_mid = 0xac25,
+        .time_high_and_version = 0x11d3,
+        .clock_seq_high_and_reserved = 0x9a,
+        .clock_seq_low = 0x2d,
+        .node = [_]u8{ 0x00, 0x90, 0x27, 0x3f, 0xc1, 0x4d },
+    };
+};
+
 pub fn main() void {
     con_out = uefi.system_table.con_out.?;
     const boot_services = uefi.system_table.boot_services.?;
@@ -25,129 +57,58 @@ pub fn main() void {
     // We're going to use this buffer to format strings.
     var buf: [100]u8 = undefined;
 
-    // Let's find some protocols that our environment provides to us.
+    const mnp align(8) = uefi.Guid{
+        .time_low = 0x7ab33a91,
+        .time_mid = 0xace5,
+        .time_high_and_version = 0x4326,
+        .clock_seq_high_and_reserved = 0xb5,
+        .clock_seq_low = 0x72,
+        .node = [_]u8{ 0xe7, 0xee, 0x33, 0xd3, 0x9f, 0x16 },
+    };
 
-    // con_out is a *SimpleTextOutputProtocol, but for the sake of this example,
-    // let's find it using locateProtocol(). All protocols have a unique GUID by
-    // which we can find them. locateProtocol() returns the first matching protocol.
-    var simple_text_output_protocol: ?*uefi.protocols.SimpleTextOutputProtocol = undefined;
-    if (boot_services.locateProtocol(&uefi.protocols.SimpleTextOutputProtocol.guid, null, @ptrCast(*?*c_void, &simple_text_output_protocol)) == uefi.status.success) {
-        puts("*** simple text output protocol is supported!\r\n");
+    var no_handles: usize = undefined;
+    var handles: [*]uefi.Handle = undefined;
+    var status: usize = undefined;
+    var i: usize = undefined;
 
-        // simple_text_output_protocol is only null if no protocol has been found.
-        // The output device may support multiple resolutions. Let's list them:
-        var i: u32 = 0;
-        while (i < simple_text_output_protocol.?.mode.max_mode) : (i += 1) {
-            var x: usize = undefined;
-            var y: usize = undefined;
-            // queryMode can fail on device error or if we request an invalid mode.
-            _ = simple_text_output_protocol.?.queryMode(i, &x, &y);
-            printf(buf[0..], "    mode {} = {}x{}\r\n", i, x, y);
+    puts("locating handles for snp...\r\n");
+    status = boot_services.locateHandleBuffer(uefi.tables.LocateSearchType.ByProtocol, &SimpleNetworkProtocol.guid, null, &no_handles, &handles);
+    printf(buf[0..], "locateHandleBuffer returned {}\r\n", status);
+    printf(buf[0..], "no_handles = {}\r\n", no_handles);
+    i = 0;
+    while (i < no_handles) : (i += 1) {
+        printf(buf[0..], "handles[{}] = {}\r\n", i, handles[i]);
+        var proto: *SimpleNetworkProtocol align(8) = undefined;
+        const s = boot_services.handleProtocol(handles[i], &SimpleNetworkProtocol.guid, @ptrCast(*?*c_void, &proto));
+        printf(buf[0..], "handleProtocol returned {}\r\n", s);
+        if (s == uefi.status.success) {
+            printf(buf[0..], "revision = {x}\r\n", proto.revision);
+            printf(buf[0..], "start() = {}\r\n", proto.start());
+            printf(buf[0..], "initialize() = {}\r\n", proto.initialize(65536, 65536));
+            printf(buf[0..], "stop() = {}\r\n", proto.stop());
         }
-    } else {
-        puts("*** simple text output protocol is NOT supported :(\r\n");
+        puts("\r\n");
     }
-    _ = boot_services.stall(3 * 1000 * 1000);
-
-    // Do we have a relative pointing device (mouse, touchpad)?
-    // NOTE: Many firmwares, including OVMF, locate a protocol here even if
-    // they don't support mice or touchscreens.
-    var simple_pointer_protocol: ?*uefi.protocols.SimplePointerProtocol = undefined;
-    if (boot_services.locateProtocol(&uefi.protocols.SimplePointerProtocol.guid, null, @ptrCast(*?*c_void, &simple_pointer_protocol)) == uefi.status.success) {
-        puts("*** simple pointer protocol is supported!\r\n");
-
-        // Check the device's resolution:
-        printf(buf[0..], "    resolution x = {} per mm\r\n", simple_pointer_protocol.?.mode.resolution_x);
-        printf(buf[0..], "    resolution y = {} per mm\r\n", simple_pointer_protocol.?.mode.resolution_y);
-        printf(buf[0..], "    resolution z = {} per mm\r\n", simple_pointer_protocol.?.mode.resolution_z);
-
-        // Does it have buttons?
-        if (simple_pointer_protocol.?.mode.left_button) {
-            puts("    has left button\r\n");
-        } else {
-            puts("    doesn't have left button\r\n");
-        }
-        if (simple_pointer_protocol.?.mode.right_button) {
-            puts("    has right button\r\n");
-        } else {
-            puts("    doesn't have right button\r\n");
-        }
-    } else {
-        puts("*** simple pointer protocol is NOT supported :(\r\n");
+    if (status == uefi.status.success) {
+        status = boot_services.freePool(@ptrCast(*[*]u8, handles));
+        printf(buf[0..], "freePool returned {}\r\n", status);
     }
-    _ = boot_services.stall(3 * 1000 * 1000);
 
-    // Do we have an absolute pointing device (touchscreen)?
-    // NOTE: see note above.
-    var absolute_pointer_protocol: ?*uefi.protocols.AbsolutePointerProtocol = undefined;
-    if (boot_services.locateProtocol(&uefi.protocols.AbsolutePointerProtocol.guid, null, @ptrCast(*?*c_void, &absolute_pointer_protocol)) == uefi.status.success) {
-        puts("*** absolute pointer protocol is supported!\r\n");
+    puts("\r\n\r\n\r\n");
+    _ = boot_services.stall(2 * 1000 * 1000);
 
-        // Check the device's resolution:
-        printf(buf[0..], "    absolute min x = {}\r\n", absolute_pointer_protocol.?.mode.absolute_min_x);
-        printf(buf[0..], "    absolute min y = {}\r\n", absolute_pointer_protocol.?.mode.absolute_min_y);
-        printf(buf[0..], "    absolute min z = {}\r\n", absolute_pointer_protocol.?.mode.absolute_min_z);
-        printf(buf[0..], "    absolute max x = {}\r\n", absolute_pointer_protocol.?.mode.absolute_max_x);
-        printf(buf[0..], "    absolute max y = {}\r\n", absolute_pointer_protocol.?.mode.absolute_max_y);
-        printf(buf[0..], "    absolute max z = {}\r\n", absolute_pointer_protocol.?.mode.absolute_max_z);
-
-        if (absolute_pointer_protocol.?.mode.attributes.supports_alt_active) {
-            puts("    supports alt active\r\n");
-        } else {
-            puts("    doesn't support alt active\r\n");
-        }
-        if (absolute_pointer_protocol.?.mode.attributes.supports_pressure_as_z) {
-            puts("    supports pressure as z\r\n");
-        } else {
-            puts("    doesn't support pressure as z\r\n");
-        }
-    } else {
-        puts("*** absolute pointer protocol is NOT supported :(\r\n");
+    puts("locating handles for mnp...\r\n");
+    status = boot_services.locateHandleBuffer(uefi.tables.LocateSearchType.ByProtocol, &mnp, null, &no_handles, &handles);
+    printf(buf[0..], "locateHandleBuffer returned {}\r\n", status);
+    printf(buf[0..], "no_handles = {}\r\n", no_handles);
+    i = 0;
+    while (i < no_handles) : (i += 1) {
+        printf(buf[0..], "handles[{}] = {}\r\n", i, handles[i]);
     }
-    _ = boot_services.stall(3 * 1000 * 1000);
-
-    // Graphics output?
-    var graphics_output_protocol: ?*uefi.protocols.GraphicsOutputProtocol = undefined;
-    if (boot_services.locateProtocol(&uefi.protocols.GraphicsOutputProtocol.guid, null, @ptrCast(*?*c_void, &graphics_output_protocol)) == 0) {
-        puts("*** graphics output protocol is supported!\r\n");
-
-        // Check supported resolutions:
-        var i: u32 = 0;
-        while (i < graphics_output_protocol.?.mode.max_mode) : (i += 1) {
-            var info: *uefi.protocols.GraphicsOutputModeInformation = undefined;
-            var info_size: usize = undefined;
-            _ = graphics_output_protocol.?.queryMode(i, &info_size, &info);
-            printf(buf[0..], "    mode {} = {}x{}\r\n", i, info.horizontal_resolution, info.vertical_resolution);
-        }
-
-        printf(buf[0..], "    current mode = {}\r\n", graphics_output_protocol.?.mode.mode);
-    } else {
-        puts("*** graphics output protocol is NOT supported :(\r\n");
+    if (status == uefi.status.success) {
+        status = boot_services.freePool(@ptrCast(*[*]u8, handles));
+        printf(buf[0..], "freePool returned {}\r\n", status);
     }
-    _ = boot_services.stall(3 * 1000 * 1000);
 
-    // What about a random number generator?
-    var rng_protocol: ?*uefi.protocols.RNGProtocol = undefined;
-    if (boot_services.locateProtocol(&uefi.protocols.RNGProtocol.guid, null, @ptrCast(*?*c_void, &rng_protocol)) == uefi.status.success) {
-        puts("*** rng protocol is supported!\r\n");
-
-        // We can pick a rng, but we're going to use the default one.
-        var lucky_number: u8 = undefined;
-        var status = rng_protocol.?.getRNG(null, 1, @ptrCast([*]u8, &lucky_number));
-        if (status == uefi.status.success) {
-            printf(buf[0..], "    your lucky number = {}\r\n", lucky_number);
-        } else {
-            // Generating random numbers can fail.
-            printf(buf[0..], "    no luck today, reason = {}\r\n", switch (status) {
-                uefi.status.unsupported => "unsupported",
-                uefi.status.device_error => "device error",
-                uefi.status.not_ready => "not ready",
-                uefi.status.invalid_parameter => "invalid parameter",
-                else => "(unknown)",
-            });
-        }
-    } else {
-        puts("*** rng protocol is NOT supported :(\r\n");
-    }
-    _ = boot_services.stall(3 * 1000 * 1000);
+    _ = boot_services.stall(10 * 1000 * 1000);
 }
